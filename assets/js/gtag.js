1a68a819bc4bbab4aef8e995d77393872b1a408f
Beshoy Mansour <beshoy.r.mansour@gmail.com>
2019-10-26 14:29:16 +0200

    Google Analytics User-ID

---
+++ assets/js/gtag.js
@@ -0,0 +1,16 @@
+function uuidv4() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c == "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+
+gtag("config", "UA-150975623-1");
+gtag("set", { user_id: uuidv4() }); // Set the user ID using signed-in user_id.
